21ec73d1679c5b733fc68ecd62733440ccb8a4fa
Joshua Mackie <113153735+FabledHero@users.noreply.github.com>
2023-12-03 17:33:36 -0600

    Update echo.go
    added error checking. Think this is correct....

--- Project2/builtins/echo.go
+++ Project2/builtins/echo.go
@@ -3,9 +3,11 @@ package builtins
 import (
 	"fmt"
 	"strings"
+	"errors"
 )
 
-func Echo(args ...string) {
+func Echo(args ...string) error {
 	message := strings.Join(args, " ")
 	fmt.Println(message)
+	return nil
 }
